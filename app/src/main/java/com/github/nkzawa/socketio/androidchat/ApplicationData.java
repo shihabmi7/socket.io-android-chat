4068e08315bbfa9677dad5a024b849ca4d39cb55
shihabmi7 <shihab.mi7@gmail.com>
2016-06-30 12:36:26 +0600

    // chat updated

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/ApplicationData.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/ApplicationData.java
@@ -4,7 +4,7 @@ public class ApplicationData {
 
     //public static final String CHAT_SERVER_URL = "http://chat.socket.io";
     public static final String CHAT_SERVER_URL = "http://192.168.5.230:3000";
-    public static  String mUserName = "";
+    public static String mUserName = "";
 
     //String URL = "http://192.168.5.230:3000";
 }
