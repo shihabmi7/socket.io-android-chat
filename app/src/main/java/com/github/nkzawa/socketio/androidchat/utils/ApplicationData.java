f2667109bf8e3dbe7e3f6aa30e63f62103e1dfde
DESKTOP-53A39RD\User <shihab.apps.developer@gmail.com>
2018-04-17 13:03:38 +0600

    First Commit in Git

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/utils/ApplicationData.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/utils/ApplicationData.java
@@ -3,7 +3,7 @@ package com.github.nkzawa.socketio.androidchat.utils;
 public class ApplicationData {
 
     //public static final String CHAT_SERVER_URL = "http://chat.socket.io";
-    public static final String CHAT_SERVER_URL = "http://192.168.0.104:3000";
+    public static final String CHAT_SERVER_URL = "http://192.168.31.84:3000";
     public static String mUserName = "";
 
     //String URL = "http://192.168.5.230:3000";
